1542fbeaf00eb475579a84388ac513354c7aa513
Kazi Efazul Karim <efazul.fayad1717@gmail.com>
2024-03-30 13:25:49 +0600

    Refactor JWT functions and middleware

--- apps/storefront/src/lib/jwt.ts
+++ apps/storefront/src/lib/jwt.ts
@@ -3,30 +3,26 @@ import { SignJWT, jwtVerify } from 'jose'
 export const signJWT = async (
    payload: { sub: string },
    options: { exp: string }
-) => {
-   try {
-      const secret = new TextEncoder().encode(process.env.JWT_SECRET_KEY)
-      const alg = 'HS256'
-      return new SignJWT(payload)
-         .setProtectedHeader({ alg })
-         .setExpirationTime(options.exp)
-         .setIssuedAt()
-         .setSubject(payload.sub)
-         .sign(secret)
-   } catch (error) {
-      throw error
-   }
+): Promise<string> => {
+   const secret = new TextEncoder().encode(process.env.JWT_SECRET_KEY)
+   const alg = 'HS256'
+   return await new SignJWT(payload)
+      .setProtectedHeader({ alg })
+      .setExpirationTime(options.exp)
+      .setIssuedAt()
+      .setSubject(payload.sub)
+      .sign(secret)
 }
 
-export const verifyJWT = async <T>(token: string): Promise<T> => {
+export const verifyJWT = async <T>(token: string): Promise<T | undefined> => {
    try {
-      return (
-         await jwtVerify(
-            token,
-            new TextEncoder().encode(process.env.JWT_SECRET_KEY)
-         )
-      ).payload as T
+      const result = await jwtVerify(
+         token,
+         new TextEncoder().encode(process.env.JWT_SECRET_KEY)
+      )
+      return result.payload as T
    } catch (error) {
       console.error({ error })
+      return undefined
    }
 }
--- apps/storefront/src/middleware.ts
+++ apps/storefront/src/middleware.ts
@@ -3,7 +3,9 @@ import { getErrorResponse } from '@/lib/utils'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(req: NextRequest) {
-   if (req.nextUrl.pathname.startsWith('/api/auth')) return NextResponse.next()
+   if (req.nextUrl.pathname.startsWith('/api/auth')) {
+      return NextResponse.next()
+   }
 
    function isTargetingAPI() {
       return req.nextUrl.pathname.startsWith('/api')
